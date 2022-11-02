b8a3bc56b50e29fc8246ed350d3f17196f39fc81
MATVEI <motyha18@gmail.com>
2022-11-02 19:09:37 +0800

    add

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-
-  pluginOptions: {
+    transpileDependencies: true,
+    publicPath: '/fronted/',
+    pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
+            // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+        }
+    }
 })
