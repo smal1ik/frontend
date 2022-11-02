f6e2c7ed6b59fe636d6c5c6a416d8712fdc4fb2d
MATVEI <motyha18@gmail.com>
2022-11-02 20:33:29 +0800

    add

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
-const app = express()
+const express = require('express');
+// const serveStatic = require('serve-static');
+// const path = require('path');
+const app = express();
+const port = process.env.PORT || 8080;
 
+// app.use('/', serveStatic(path.join(__dirname, '/dist')));
+//
+// app.get(/.*/, function (req, res) {
+//     res.sendFile(path.join(__dirname, '  /dist/index.html'));
+// });
+app.use(express.static( __dirname + "/dist/"));
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.get(/.*/, function(reg, res) {
+    res.sendfile(__dirname + "/dist/index.html");
+});
 
-app.get(/.*/, function (req, res) {
-    res.sendFile(path.join(__dirname, '/dist/index.html'))
-})
-
-const port = process.env.PORT || 8080
-app.listen(port)
+app. listen(port);
 console. log(`app is listening on port: ${port}`)
