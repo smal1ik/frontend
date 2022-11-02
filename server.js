cf319ab4fdb2f5fb07a3e92e5ebeadcf93c1199b
MATVEI <motyha18@gmail.com>
2022-11-02 20:43:09 +0800

    add

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 const express = require('express');
-// const serveStatic = require('serve-static');
-// const path = require('path');
+const serveStatic = require('serve-static');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-// app.use('/', serveStatic(path.join(__dirname, '/dist')));
-//
-// app.get(/.*/, function (req, res) {
-//     res.sendFile(path.join(__dirname, '  /dist/index.html'));
-// });
-app.use(express.static( __dirname + "/dist/"));
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
 
-app.get(/.*/, function(reg, res) {
-    res.sendfile(__dirname + "/dist/index.html");
+app.get(/.*/, function (req, res) {
+    res.sendFile(path.join(__dirname, '  /dist/index.html'));
 });
 
 app. listen(port);
-console. log(`app is listening on port: ${port}`)
+console. log(`app is listening on port: ${port}`);
